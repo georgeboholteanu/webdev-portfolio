b40835076d88b4afbdde04ee0b78fd2938410351
georgeboholteanu <george.boholteanu@gmail.com>
2023-12-19 00:55:09 +0000

    routing. socials. mobileNav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -40,38 +40,6 @@ const Nav = ({ containerStyles, linkStyles, underlineStyles }) => {
 				);
 			})}
 		</nav>
-		// <div
-		// 	className={`flex-1 justify-self-center pb-3 mt-8 md:block md:pb-0 md:mt-0 ${
-		// 		navbar ? "p-12 md:p-0 block" : "hidden"
-		// 	}`}
-		// >
-		// 	<ul className="h-screen md:h-auto items-center justify-center md:flex bg-slate-300 md:bg-transparent">
-		// 		<li className="text-gray-200 font-sans pb-3 text-xl py-2 px-6 text-center hover:underline md:hover:text-yellow-300 md:hover:bg-transparent">
-		// 			<Link href="/Home" onClick={() => setNavbar(!navbar)}>
-		// 				Home
-		// 			</Link>
-		// 		</li>
-		// 		<li className="text-gray-200 font-sans pb-3 text-xl py-2 px-6 text-center hover:underline md:hover:text-yellow-300 md:hover:bg-transparent">
-		// 			<Link href="/about" onClick={() => setNavbar(!navbar)}>
-		// 				About
-		// 			</Link>
-		// 		</li>
-		// 		<li className="text-gray-200 font-sans pb-3 text-xl py-2 px-6 text-center hover:underline md:hover:text-yellow-300 md:hover:bg-transparent">
-		// 			<Link href="/projects" onClick={() => setNavbar(!navbar)}>
-		// 				Projects
-		// 			</Link>
-		// 		</li>
-		// 		<li className="text-gray-200 font-sans pb-3 text-xl py-2 px-6 text-center hover:underline md:hover:text-yellow-300 md:hover:bg-transparent">
-		// 			<Link
-		// 				className="pr-20"
-		// 				href="/contact"
-		// 				onClick={() => setNavbar(!navbar)}
-		// 			>
-		// 				Contact
-		// 			</Link>
-		// 		</li>
-		// 	</ul>
-		// </div>
 	);
 };
 
