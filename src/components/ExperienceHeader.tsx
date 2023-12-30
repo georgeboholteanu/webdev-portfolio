42e53fd038a90146d3c78dbe5b09813df2d96bf1
georgeboholteanu <george.boholteanu@gmail.com>
2023-12-30 01:12:00 +0000

    fixed TailwindIcon

--- src/components/ExperienceHeader.tsx
+++ src/components/ExperienceHeader.tsx
@@ -16,38 +16,34 @@ import { IoLogoJavascript } from "react-icons/io";
 import TailwindIcon from "@/components/TailwindIcon";
 
 const iconsFrameworks = [
-	{ name: <IoLogoJavascript /> },
-	{ name: <FaPython /> },
-	{ name: <FaHtml5 /> },
-	{ name: <FaReact /> },
-	{ name: <FaCss3Alt /> },
-	{
-		name: (
-			<i className="border border-black dark:border-white rounded-md text-gray-800 dark:text-white justify-center items-center">
-				<TailwindIcon className="w-[1.3rem] md:w-[1.4rem] lg:w-[1.8rem]" />
-			</i>
-		),
-	},
+	{ name: <IoLogoJavascript />, key: "javascript" },
+	{ name: <FaPython />, key: "python" },
+	{ name: <FaHtml5 />, key: "html5" },
+	{ name: <FaReact />, key: "react" },
+	{ name: <FaCss3Alt />, key: "css" },
+	{ name: <TailwindIcon />, key: "tailwind" },
 ];
+
 const iconsTools = [
-	{ name: <FaFigma /> },
-	{ name: <SiAdobephotoshop /> },
-	{ name: <SiAdobeindesign /> },
-	{ name: <SiAdobeillustrator /> },
+	{ name: <FaFigma />, key: "figma" },
+	{ name: <SiAdobephotoshop />, key: "photoshop" },
+	{ name: <SiAdobeindesign />, key: "indesign" },
+	{ name: <SiAdobeillustrator />, key: "illustrator" },
 ];
 
 const ExperienceHeader = () => {
-	
 	return (
 		<div className="mb-10 flex flex-col gap-4 justify-between ">
 			{/* frameworks */}
 			<div className="flex items-center">
-				<h4 className="text-sm md:text-xl uppercase w-[160px] sm:w-[200px] text-yellow-500 ">Frameworks</h4>
+				<h4 className="text-sm md:text-xl uppercase w-[160px] sm:w-[200px] text-yellow-500 ">
+					Frameworks
+				</h4>
 				<div className="flex gap-x-2 text-md md:text-lg lg:text-2xl xl:text-3xl items-center pl-4">
-					{iconsFrameworks.map((icon, index) => (
+					{iconsFrameworks.map((icon) => (
 						<i
 							className="flex flex-1 text-gray-800 dark:text-white"
-							key={index}
+							key={icon.key}
 						>
 							{icon.name}
 						</i>
@@ -65,12 +61,14 @@ const ExperienceHeader = () => {
 			</div>
 			{/* tools */}
 			<div className="flex items-center">
-				<h4 className="text-sm md:text-xl uppercase w-[160px] sm:w-[200px] text-yellow-500">Tools</h4>
+				<h4 className="text-sm md:text-xl uppercase w-[160px] sm:w-[200px] text-yellow-500">
+					Tools
+				</h4>
 				<div className="flex gap-x-2 text-md md:text-lg lg:text-2xl xl:text-3xl items-center pl-3">
-					{iconsTools.map((icon, index) => (
+					{iconsTools.map((icon) => (
 						<i
 							className="flex flex-1 text-gray-800 dark:text-white"
-							key={index}
+							key={icon.key}
 						>
 							{icon.name}
 						</i>
